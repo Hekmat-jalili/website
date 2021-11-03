9dfa9c19eca38aafe7ed44c4029ccfa25e65c2ae
hekmat jalili <93056599+Hekmat-jalili@users.noreply.github.com>
2021-11-03 14:55:21 +0430

    Add files via upload

---
+++ js.js
@@ -0,0 +1,13 @@
+function Change() {
+
+    var create = document.createElement("img");
+    create.setAttribute("type", "img");
+    create.setAttribute("value", "hekmat/warning.png");
+    var firstCreateTheVar = document.getElementById("image");
+    firstCreateTheVar.appendChild(create);
+
+
+
+
+
+}
